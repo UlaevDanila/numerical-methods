270e8e18fd60ab49284b811d100b76a35f6df3e9
UlaevDanila <80589144+UlaevDanila@users.noreply.github.com>
2022-03-21 02:19:12 +0200

    Add files via upload

--- Interface.py
+++ Interface.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPlainTextEdit, QPushButton, QVBoxLayout
-import sys 
+import PyQt5.QtWidgets
+from PyQt5.QtWidgets import *
+import sys
 from FunctionalRealisation import main_calc
 
 class Main(QWidget):
@@ -21,6 +22,10 @@ class Main(QWidget):
         self.qle5.setPlaceholderText('Результат')
         btn = QPushButton("Решить", self)
         btn.clicked.connect(self.btnclick)
+        self.table = QTableWidget(self)
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(['x', 'y.приб', 'y.точн', 'погрешность'])
+        
 
         lo = QVBoxLayout(self)
 
@@ -30,6 +35,7 @@ class Main(QWidget):
         lo.addWidget(self.qle4)
         lo.addWidget(self.qle5)
         lo.addWidget(btn)
+        lo.addWidget(self.table)
 
     def btnclick(self):
         a = self.qle1.text()
@@ -38,7 +44,28 @@ class Main(QWidget):
         y0 = self.qle4.text()
         try:
             if (a is not None) or (b is not None) or (epsilon is not None) or (y0 is not None):
-                self.qle5.setPlainText(str(main_calc(float(a), float(b), float(epsilon), float(y0))))
+                x, y, y1 = main_calc(float(a), float(b), float(epsilon), float(y0))
+                delta = abs(y-y1)
+                self.table.setRowCount(len(x))
+                n = 0
+                for i in x:
+                    self.table.setItem(n, 0, QTableWidgetItem(str(round(i,8))))
+
+                    n += 1
+                n = 0
+                for i in y:
+                    self.table.setItem(n, 1, QTableWidgetItem(str(round(i,8))))
+                    n += 1
+                n = 0
+                for i in y1:
+                    self.table.setItem(n, 2, QTableWidgetItem(str(round(i,8))))
+                    n += 1
+                n = 0
+                for i in delta:
+                    self.table.setItem(n, 3, QTableWidgetItem(str(round(i,8))))
+                    n += 1
+
+                self.qle5.setPlainText(str(x))
             else:
                 self.qle5.clear()
                 self.qle5.setPlainText('Заполни все поля')
